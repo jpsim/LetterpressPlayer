3dcde4f229d8f85be48c8b4ed90f4359902718ad
JP Simard <jp@jpsim.com>
2013-02-18 19:55:00 -0500

    Added a help message on first launch

--- LetterpressPlayer/MBAppDelegate.m
+++ LetterpressPlayer/MBAppDelegate.m
@@ -12,6 +12,13 @@
 @implementation MBAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    if (![[NSUserDefaults standardUserDefaults] boolForKey:@"HasLaunchedOnce"]) {
+        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"HasLaunchedOnce"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+        
+        [[[UIAlertView alloc] initWithTitle:@"Welcome" message:@"To use this app, take a screenshot of a Letterpress game and tap \"Analyze\".\n\nTap \"Refresh\" to update the score of game you last analyzed." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+    }
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[MBViewController alloc] init]];
     [self.window makeKeyAndVisible];
